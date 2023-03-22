dc26fd5f70bcae4376cf2597c3c249d31a2b7ea1
Jhonny Mosquera <78888142+jhonnymosquera@users.noreply.github.com>
2023-03-21 21:34:21 -0500

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Redes from "../components/Redes";
+
+function HomePage() {
+  return (
+    <main>
+      <header>
+        <Redes />
+      </header>
+    </main>
+  );
+}
+
+export default HomePage;
