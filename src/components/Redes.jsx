0dfc8100224532f3d3e93833054404704dc9a7d4
jhonnymosquera <78888142+jhonnymosquera@users.noreply.github.com>
2023-03-21 23:19:57 -0500

    Portafolio

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function Redes() {
   return (
     <div className="redes">
-          <p>Jhonny Mosquera</p>
+      <div className="redes_nombre">
+        <p>Jhonny Mosquera</p>
+      </div>
 
-          <div>
-            <a
-              href="https://github.com/JhonnyMosquera"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
+      <div className="redes_icono">
+        <a
+          href="https://github.com/JhonnyMosquera"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github"></i>
+        </a>
 
-            <a
-              href="https://www.linkedin.com/in/jhonny-mosquera-a0a14111b/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a>
-            
-            <a
-              href="https://twitter.com/jhonnymosquera_"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </div>
-        </div>
-  )
+        <a
+          href="https://www.linkedin.com/in/jhonny-mosquera-a0a14111b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+
+        <a
+          href="https://twitter.com/jhonnymosquera_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-twitter"></i>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Redes
+export default Redes;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,17 @@ import Redes from "../components/Redes";
 function HomePage() {
   return (
     <main>
-      <header>
+      <header className="header">
         <Redes />
+
+        <div className="photo">
+          <img src="jhonny_photo.jpeg" alt="" />
+        </div>
+
+        <div className="descripcion">
+        <h1>Junior Frontend</h1>
+        <p></p>
+        </div>
       </header>
     </main>
   );
