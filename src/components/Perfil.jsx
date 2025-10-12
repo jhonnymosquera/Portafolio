0b8a7249d68de9d1003dbe437ed3b0ec73135b2d
jhonnymosquera <jhonnymosquera.web@gmail.com>
2025-10-12 13:08:13 -0500

    [ADD] Added Jhonny's profile photo and updated profile description

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,6 +1,5 @@
 import useContexApp from '../Context/useContextApp';
 import Contact_button from './Contact_button';
-import Picture from './utils/Picture';
 
 export default function Perfil() {
 	let { perfil } = useContexApp();
@@ -10,7 +9,7 @@ export default function Perfil() {
 	return (
 		<div className="perfil">
 			<div className="perfil_photo">
-				<Picture img={photo} className={'perfil_photo_img'} />
+				<img src={'perfil/jhonny_photo.png'} style={{ borderRadius: '25%' }} className={'perfil_photo_img'} alt={photo.alt} />
 			</div>
 
 			<div className="perfil_desc">
